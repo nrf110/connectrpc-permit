8abf311fdf0bffcf5f68d6b5e5291e0d90d1ec9c
Nick Fisher <nfisher110@gmail.com>
2024-10-16 11:33:52 +0000

    attempt to auto-configure git in devcontainer

--- .devcontainer/gitconfig.sh
+++ .devcontainer/gitconfig.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
 rm -f git.env
-echo "GIT_EMAIL=\"$(git config get --global user.email)\"" >> git.env
-echo "GIT_NAME=\"$(git config get --global user.name)\"" >> git.env
+echo "GIT_AUTHOR_EMAIL=\"$(git config get --global user.email)\"" >> .devcontainer/git.env
+echo "GIT_COMMITTER_EMAIL=\"$(git config get --global user.email)\"" >> .devcontainer/git.env
+echo "GIT_AUTHOR_NAME=\"$(git config get --global user.name)\"" >> .devcontainer/git.env
+echo "GIT_COMMITTER_NAME=\"$(git config get --global user.name)\"" >> .devcontainer/git.env
